cff1dfdb51bce0c82641c163588bbcb8ba4345ce
IlyaKaplan2002 <kaplan11092002@gmail.com>
2022-08-02 11:32:26 +0300

    ...

--- js/ar.js
+++ js/ar.js
@@ -15,55 +15,6 @@ const previewImage = document.querySelector("#previewImage");
 const tweet = document.querySelector("#tweet");
 const back = document.querySelector("#back");
 
-// function captureVideoFrame(video, format, width, height) {
-//   if (typeof video === "string") {
-//     video = document.querySelector(video);
-//   }
-
-//   format = format || "jpeg";
-
-//   if (!video || (format !== "png" && format !== "jpeg")) {
-//     return false;
-//   }
-
-//   var canvas = document.createElement("CANVAS");
-
-//   canvas.width = width || video.videoWidth;
-//   canvas.height = height || video.videoHeight;
-//   canvas.getContext("2d").drawImage(video, 0, 0);
-//   var dataUri = canvas.toDataURL("image/" + format);
-//   var data = dataUri.split(",")[1];
-//   var mimeType = dataUri.split(";")[0].slice(5);
-
-//   var bytes = window.atob(data);
-//   var buf = new ArrayBuffer(bytes.length);
-//   var arr = new Uint8Array(buf);
-
-//   for (var i = 0; i < bytes.length; i++) {
-//     arr[i] = bytes.charCodeAt(i);
-//   }
-
-//   var blob = new Blob([arr], { type: mimeType });
-//   return {
-//     blob: blob,
-//     dataUri: dataUri,
-//     format: format,
-//     width: canvas.width,
-//     height: canvas.height,
-//   };
-// }
-
-// function resizeCanvas(origCanvas, width, height) {
-//   let resizedCanvas = document.createElement("canvas");
-//   let resizedContext = resizedCanvas.getContext("2d");
-
-//   resizedCanvas.height = height * 0.6;
-//   resizedCanvas.width = width;
-
-//   resizedContext.drawImage(origCanvas, 0, 0, width, height);
-//   return resizedCanvas.toDataURL();
-// }
-
 function resizeCanvas(origCanvas, width, height) {
   let resizedCanvas = document.createElement("canvas");
   let resizedContext = resizedCanvas.getContext("2d");
@@ -127,7 +78,10 @@ async function takeScreen() {
   let frame = captureVideoFrame("video", "png");
   aScene = resizeCanvas(aScene, frame.width, frame.height);
   frame = frame.dataUri;
-  const image = await mergeImages([frame, aScene]).then((b64) => {
+  const image = await mergeImages([frame, aScene], {
+    width: 1.5 * frame.width,
+    height: 1.5 * frame.height,
+  }).then((b64) => {
     return b64;
   });
 
